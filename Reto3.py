eeda3e30b625bbe192a8bc865f2dfe23ad20e22c
Bryan Alexander Campuzano <bryan.campuzano@misena.edu.co>
2022-05-20 01:42:26 -0500

    commit de actualizacion del 20 de mayo

--- Reto3.py
+++ Reto3.py
@@ -26,11 +26,8 @@ ventas3 = [
     (2564,'Cárter' , 'PT29872',2,32 ,'Peter Cerezo',8545436,'14/06/2020'), 
     (5412,'válvula', 'AZ8798' ,2,11 ,'Juan Peña'   ,568975 ,'14/06/2020')]
 
-#-----------------DECLARACIÓN DE DICCIONARIOS------------------
-ventas00 = {}
-
-#------------------------ZONA DE CÓDIGO------------------------
-
+#------------------------ZONA DE CÓDIGO 1------------------------
+#------------------------SOLUCIÓN 1 (NOTA OBTENIDA: 4,17)------------------------
 """ este método toma una lista de tuplas referente al registro de cada venta realizada y genera un diccionario
     con todas estas ventas indexadas 
         parametros: ventas(List): lista que contiene las tuplas con la informacion de cada venta realizada
@@ -59,21 +56,49 @@ def consultaRegistro(ventas, idProducto):
     cantidadRetorno = 0
     dicTemp = AutoPartes(ventas)
     for i in range(0,len(dicTemp)):
-        venta = (dicTemp[i])
-        if (venta[0] == idProducto): 
-            temp = f"Producto consultado : {venta[0]} Descripción {venta[1]} #Parte {venta[2]} Cantidad vendida {venta[3]} Stock {venta[4]} Comprador {venta[5]} Documento {venta[6]} Fecha Venta {venta[7]}"
-            print(temp)
-        elif(cantidadRetorno == 0):
-            cantidadRetorno += 1
-        elif (temp =="" and cantidadRetorno != 0):
-            temp = "No hay registro de venta de ese producto" 
-            print(temp)
-        else:
-            pass
-#-------------------------ZONA DE TEST-------------------------
-print("----------------separador-------------@------")
-consultaRegistro(ventas1,2010)
-consultaRegistro(ventas2,2001)
-consultaRegistro(ventas3,9852)
-print("----------------separador--------------@-----")
+            venta = (dicTemp[i])
+            if (venta[0] == idProducto): 
+                temp = f"Producto consultado : {venta[0]}  Descripción  {venta[1]}  #Parte  {venta[2]}  Cantidad vendida  {venta[3]}  Stock  {venta[4]}  Comprador {venta[5]}  Documento  {venta[6]}  Fecha Venta  {venta[7]}"
+                print(temp)
+            elif(cantidadRetorno == 0):
+                cantidadRetorno += 1
+            elif (temp =="" and cantidadRetorno != 0):
+                temp = "No hay registro de venta de ese producto" 
+                print(temp)
+            else:
+                pass    
+           
+#-------------------------ZONA DE TEST 1-------------------------
+#print("----------------separador-------------@------")
+#print(consultaRegistro(ventas1,2010))
+#print(consultaRegistro(ventas2,2001))
+#print(consultaRegistro(ventas3,9852))
+#print("----------------separador--------------@-----")
+
+#------------------------SOLUCIÓN 2 (NOTA: PENDIENTE)------------------------
+#------------------------ZONA DE CÓDIGO 2------------------------
+""" este método toma una lista de tuplas referente al registro de cada venta realizada y genera un diccionario
+    con todas estas ventas indexadas 
+        parametros: ventas(List): lista que contiene las tuplas con la informacion de cada venta realizada
+        
+        retorna: diccionarioTemp(dict): diccionario con los datos de las ventas, indexado con llaves para su organización
+"""
+def AutoPartes2(ventas: list):
+    pass
+
+""" este método recibe como parametro una lista, la cual es pasada por el método AutoPartes() para su indexacion
+    ademas de un entero idProducto, numero de identificación único por pieza y retorna todas las ventas realizadas
+    de este producto, mensaje de error de no encontrar ninguna venta realizada bajo este id de producto
+        parametros: ventas(List): lista que contiene las tuplas con la informacion de cada venta realizada
+                    idProducto(int): numero de identificación único por pieza
+        retorna:    temp(str): string de la forma 
+                    'Producto consultado : IdProducto Descripción dProducto #Parte pnProducto Cantidad vendida cvProducto Stock sProducto Comprador nComprador Documento cComprador Fecha Venta fVenta' 
+                    de encontrar ventas bajo el id proporcionado, o un string de la forma 
+                    'No hay registro de venta de ese producto'
+                    cuando no encuentra ventas bajo el id proporcionado
+"""
+def consultaRegistro2(ventas, idProducto):
+    pass
+
+#-------------------------ZONA DE TEST 2-------------------------
 #   appEnd
